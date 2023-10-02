7584fe6a36ca1d5424b7ea807f8a1fce9a652704
oosh70 <oosh70@gmail.com>
2023-10-02 21:08:19 +0900

    공지사항부분 crud / create 부분 오류 수정
    t# with '#' will be ignored, and an empty message aborts the commit.

--- src/main/java/com/example/yoonlove/service/CreatorService.java
+++ src/main/java/com/example/yoonlove/service/CreatorService.java
@@ -1,6 +1,9 @@
 package com.example.yoonlove.service;
 
+import com.example.yoonlove.Dto.CreatorDto;
+import com.example.yoonlove.mapper.CreatorMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -9,7 +12,7 @@ import java.util.List;
 public class CreatorService {
     //Dto 객체 주입
     @Autowired
-    private CreatorDto creatorMapper;
+    private CreatorMapper creatorMapper;
 
     //서비스 메소드 작성 CRUD
     public CreatorDto selectCreator(){
