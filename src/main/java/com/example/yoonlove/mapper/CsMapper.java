216f07ba5ade25f17c16b8b099eb303e3354242b
oosh70 <oosh70@gmail.com>
2023-10-03 20:34:39 +0900

    qna 메소드 추가

--- src/main/java/com/example/yoonlove/mapper/CsMapper.java
+++ src/main/java/com/example/yoonlove/mapper/CsMapper.java
@@ -1,15 +1,25 @@
 package com.example.yoonlove.mapper;
 
 import com.example.yoonlove.Dto.NoticeDto;
+import com.example.yoonlove.Dto.QnADto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface CsMapper {
+    //공지사항 메소드
     public List<NoticeDto> selectListNotice();
     public NoticeDto selectNotice();
     public void insertNotice();
     public void updateNotice();
-    public void deleteNotice();
+
+
+
+    //QnA 메소드
+    public List<QnADto> selectListQnA();
+    public QnADto selectQnA();
+    public void insertQnA();
+    public void updateQnA();
+    public void deleteQnA();
 }
