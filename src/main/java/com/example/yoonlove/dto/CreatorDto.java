5e469b2b31682f88184497a33bb21d9dc3fddab0
yunjungeun <dev.byje@gmail.com>
2023-10-10 20:09:54 +0900

    크리에이터 crud 작성/검증 완료 1010

--- src/main/java/com/example/yoonlove/dto/CreatorDto.java
+++ src/main/java/com/example/yoonlove/dto/CreatorDto.java
@@ -10,4 +10,14 @@ public class CreatorDto {
     private String ch_name;
     private int ch_sub;
     private String user_id;
+
+    @Override
+    public String toString() {
+        return "CreatorDto{" +
+                "ch_id='" + ch_id + '\'' +
+                ", ch_name='" + ch_name + '\'' +
+                ", ch_sub=" + ch_sub +
+                ", user_id='" + user_id + '\'' +
+                '}';
+    }
 }
