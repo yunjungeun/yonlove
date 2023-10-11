5e68b5b67975a9a2c5629319965f8217b9bad387
oosh70 <oosh70@gmail.com>
2023-10-11 14:32:48 +0900

    페이징 테스트중

--- src/main/java/com/example/yoonlove/dto/PageDto.java
+++ src/main/java/com/example/yoonlove/dto/PageDto.java
@@ -13,9 +13,9 @@ public class PageDto {
     private int postStart; //게시판의 시작 게시글 번호
     private int postEnd; //게시판의 마지막 게시글 번호
 
-    private int pageCnt = 10; //페이지의 총 갯수 값
-    private int currentPage; //현재 페이지
-    private int paging; //게시글 밑 페이징에서 표시될 페이지의 수 값
+    private int pageCnt; //페이지의 총 갯수 값
+    private int currentPage = 1; //현재 페이지
+    private int paging = 10; //게시글 밑 페이징에서 표시될 페이지의 수 값
     private int pageStart; //페에징에서 시작 페이지 값
     private int pageEnd; //페이징에서 마지막 값
 
--- src/main/java/com/example/yoonlove/utill/PageUtill.java
+++ src/main/java/com/example/yoonlove/utill/PageUtill.java
@@ -19,12 +19,24 @@ public class PageUtill {
     }
 
     //총 페이지를 구하는 방법  = // (전체글 -1) / 페이징 갯수   + 1
-    public int totalPageCnt(PageDto dto){
+    public int totalPageCnt(){
         PageDto totalPage = new PageDto();
 
         //(전체글 -1) / 페이징 갯수   + 1
-        int pageCnt =(totalPost()-1)/totalPage.getPageCnt() + 1;
+        int pageCnt =(totalPost()-1)/totalPage.getPaging() + 1;
 
         return pageCnt;
     }
+
+    //페이징에서 시작 페이지 구하는 법 // (현재페이지 번호 -1 / 페이징사이즈)* 페이징 사이즈 + 1 =시작번호
+    //현재페이지값은 setter나 객체로 넣고, 기본값은 0임. 페이징사이즈 기본값은 10임.
+    public int pageStart(){
+        PageDto pageDto = new PageDto();
+        int result = ((pageDto.getCurrentPage() - 1) / pageDto.getPaging()) * pageDto.getPaging() + 1;
+        return result;
+    }
+
+    //페이징에서 끝 페이지 구하는법 // 시작페이지 + 페이징 사이즈 - 1 과 총페이지보다 작은것
+    public int
+
 }
