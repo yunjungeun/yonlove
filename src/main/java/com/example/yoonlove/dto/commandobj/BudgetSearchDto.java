437f03994cab561b17bdea168858b9e82f218173
yunjungeun <dev.byje@gmail.com>
2023-10-16 12:47:59 +0900

    1016_검색 주석 추가

--- src/main/java/com/example/yoonlove/dto/commandobj/BudgetSearchDto.java
+++ src/main/java/com/example/yoonlove/dto/commandobj/BudgetSearchDto.java
@@ -10,4 +10,5 @@ import lombok.Setter;
 public class BudgetSearchDto {
     private String name;
     private String projectId;
+    //검색기능- DB 에서 이름과 아이디를 찾기위해 따로 DTO를 만듬
 }
--- src/main/java/com/example/yoonlove/dto/commandobj/BudgetSearchDto2.java
+++ src/main/java/com/example/yoonlove/dto/commandobj/BudgetSearchDto2.java
@@ -1,6 +1,6 @@
 package com.example.yoonlove.dto.commandobj;
 
-public class BudgetSearchDto2 {
+public class BudgetSearchDto2 {  // 수업때 검색기능 배웠던 버전
     private String searchType;
     /*
         projectId
