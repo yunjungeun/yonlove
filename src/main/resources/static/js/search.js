3d85fe6267c26fb876db1be0b49bedd84282d4ce
oosh70 <oosh70@gmail.com>
2023-10-21 15:58:32 +0900

    공지사항 검색 js 최적화

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,38 +1,19 @@
     // search 할때, 기본 URL 주소값을 변경하기 위해..
     function submitForm() {
+        var baseUrl = url
+
         //  위의 select의 name인 서치타입을 가져오고 해당 입력값을 가져옴
         var searchTypeSelect = document.getElementById("searchType");
         var selectedSearchType = searchTypeSelect.value;
-
-        //인풋 값을 컨텐츠에 담음
         var search = document.getElementById("search").value;
 
 
         // 프로젝트Id 로 검색 또는 입력값이 없을때
-        if (selectedSearchType === "title" && search !== "") {  // 검색시, projectId와 빈칸일때
-            // URL 주소를 아래로 변경해라 (베이스URL / 서치 URL)
-            var baseUrl = "/cs/notice";
-            var searchUrl = baseUrl + "?title=" + search;
-
-            //  searchUrl로 이동
+        if (search !== "") {
+            var searchUrl = baseUrl + "?"+selectedSearchType+"=" + search;
             window.location.href = searchUrl;
-        }
-
-        if (selectedSearchType === "writer" && search !== "") {
-            // Construct the URL
-            var baseUrl = "/cs/notice";
-            var searchUrl = baseUrl + "?writer=" + search;
-
-
-            window.location.href = searchUrl;
-        }
-        if (selectedSearchType === "all" && search == "") {
-            // Construct the URL
-            var baseUrl = "/cs/notice";
-            var searchUrl = baseUrl;
-
-
-            window.location.href = searchUrl;
+        }else{
+            window.location.href = baseUrl;
         }
 
         return false;
