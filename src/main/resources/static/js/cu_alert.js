73d6cfff744913bbe615d6eb1549829c1a3f3afd
oosh70 <oosh70@gmail.com>
2023-10-24 12:28:03 +0900

    cu_alert.js 인트값 주석

--- src/main/resources/static/js/cu_alert.js
+++ src/main/resources/static/js/cu_alert.js
@@ -14,16 +14,15 @@ function mappingurl(submiturl,sucessurl, int, text, cnt){
             });
         }
 
+        //숫자형 name값을 처리하는 for문
         for(var j = 1; j<= cnt; j++){
-            var intname = ".int"+j;
-            $(intname).each(function(){
-            var intField = $(this).attr("name");
-            data[intField] = parseInt($(this).val());
-            });
+            var intname = ".int"+j;     //int+j 클래스들을 intname 변수에 저장
+            $(intname).each(function(){  //각 클래스마다 다음 함수를 반복실행
+            var intField = $(this).attr("name");  //intField 변수에 name의 값(dto의 프로퍼티)을 저장
+            var parsedValue = parseInt($(this).val());   //사용자가 입력한 값을 숫자로 변경하여 parseValue에 저장
+            data[intField] = isNaN(parsedValue) ? 0 : parsedValue;   //parseValue가 null값이면 0으로 수정하여 저장
+            });                                                         //.val() 함수는 null값이 들어왔을때 ""형태로 무조건 문자열로 반환, dto의 int=""; 가되서 오류 발생
         }
-
-
-
 
                 // 옵션값이 존재하는 확인하는 if문
     if(fkid != null){
