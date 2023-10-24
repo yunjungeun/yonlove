6c87f9b7444556781399ff02956966e2992ec834
oosh70 <oosh70@gmail.com>
2023-10-24 11:42:07 +0900

    cu_alert.js가 작동안됨

--- src/main/resources/static/js/cu_alert.js
+++ src/main/resources/static/js/cu_alert.js
@@ -1,26 +1,38 @@
 //html 본문에서 옵션 값을 받아와 저장하는 변수
 var fkid = selectName;
-function mappingurl(submiturl,sucessurl, int, text){
+function mappingurl(submiturl, sucessurl, int, text, cnt){
     document.getElementById("alert").addEventListener("submit", function (event){
     event.preventDefault();
 
+    alert("dd")
+    console.log("1")
     var data = {};
 
         for(var i = 1; i <= int; i++){
+        console.log("2")
             var name = ".field"+i;
             $(name).each(function() {
              var field = $(this).attr("name");
-                //val()는 항상 문자열로 값을 가져옴
-                if (!isNaN($(this).val())) {
-                    data[field] = parseInt($(this).val()); // 숫자형으로 변환하여 저장
-                } else {
-                    data[field] = $(this).val(); // 문자열로 저장
-                }
+             data[field] = $(this).val(); // 문자열로 저장
+
             });
+        }
+        console.log("3")
+        if(cnt!=0){
+        console.log("4")
+            for(var j= 1; j <= cnt; j++){
+                var intcnt = ".int"+j;
+                $(intcnt).each(function(){
+                var intfield = $(this).attr("name");
+                data[intfield] = parseInt(  if($(this).val()!=null){
+                                                $(this).val();
+                                                }else{"0";}); //숫자형으로 변환
+                });
+            }
         }
 
 
-                // 옵션값이 존재하는 확인하는 if문
+        // 옵션값이 존재하는 확인하는 if문
     if(fkid != null){
         var selectedValue = $("#fk").val(); // #fk는 select 요소의 ID입니다.
         data[fkid] = selectedValue;
