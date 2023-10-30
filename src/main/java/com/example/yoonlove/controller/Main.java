d0487d51aab686d5fd3c246ee3cfd329b063365b
oosh70 <oosh70@gmail.com>
2023-10-30 15:04:42 +0900

    css

--- src/main/java/com/example/yoonlove/controller/Main.java
+++ src/main/java/com/example/yoonlove/controller/Main.java
@@ -8,7 +8,17 @@ public class Main {
 
     @GetMapping("/index")
     public String mainPage(){
+        return "/main/index2";
+    }
+
+
+    @GetMapping("/임시")
+    public String mainPage2(){
         return "/main/index";
     }
 
+    @GetMapping("/게시판")
+    public String testPage1(){
+        return "/main/posttest";
+    }
 }
