c5f740776c4ea567332ed57c6c8cc2681aca6431
yunjungeun <dev.byje@gmail.com>
2023-10-30 18:46:20 +0900

    1030_달력폼 (출력 오류)

--- src/main/java/com/example/yoonlove/controller/CalendarController.java
+++ src/main/java/com/example/yoonlove/controller/CalendarController.java
@@ -1,64 +1,50 @@
-/*
 package com.example.yoonlove.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-public class CalendarController {
-
-    @GetMapping("/calendar")
-    public ModelAndView showCalendar() {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("/layout/newcal");
-        return mv;
-    }
-}
-
-*/
-
-
-package com.example.yoonlove.controller;
-
-import com.example.yoonlove.dto.CalendarEventDto;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
+
 @Controller
 public class CalendarController {
+
     @GetMapping("/calendar")
-    public ModelAndView showCalendar(){
-        ModelAndView mv= new ModelAndView();
-        // 달력 데이터 생성
-        List<CalendarEventDto> events = createCalendarData();
+    public String showCalendar(Model model) {
+        // 현재 날짜 가져오기
+        LocalDate currentDate = LocalDate.of(2023, 10, 1);
 
-        mv.setViewName("/layout/calendar");
-        mv.addObject("event", events);
-        return mv;
-    }
+        // 한국 날짜 형식으로 출력하기 위한 포맷터
+        DateTimeFormatter koreanDateFormatter = DateTimeFormatter.ofPattern("yyyy 년 MM 월 dd 일");
 
-    private List<CalendarEventDto> createCalendarData(){
-        // 달력 데이터 생성 로직
-        List<CalendarEventDto> events = new ArrayList<>();
-        for (int month = 1; month <= 12; month++) {
-            for (int day = 1; day <= daysInMonth(month); day++) {
-                events.add(new CalendarEventDto(Calendar.getInstance().get(Calendar.YEAR), month, day, "Sample Event"));
-            }
+        // 일주일의 요일 목록
+        String[] dayOfWeek = {"일", "월", "화", "수", "목", "금", "토"};
+
+        // 현재 월의 첫 날의 요일 구하기 (0: 일요일, 1: 월요일, ..., 6: 토요일)
+        int startDayOfWeek = currentDate.withDayOfMonth(1).getDayOfWeek().getValue();
+
+        // 현재 월의 마지막 날짜 구하기
+        int lastDayOfMonth = currentDate.lengthOfMonth();
+
+        // 현재 월의 달력 생성
+        List<String> calendar = new ArrayList<>();
+        for (int day = 1; day <= lastDayOfMonth; day++) {
+            // 날짜와 요일을 한국 날짜 형식으로 포맷하여 목록에 추가
+            String formattedDate = currentDate.format(koreanDateFormatter) + " " + dayOfWeek[startDayOfWeek];
+            calendar.add(formattedDate);
+
+            // 다음 날짜로 이동
+            startDayOfWeek = (startDayOfWeek + 1) % 7; // 다음 요일 계산
+            currentDate = currentDate.plusDays(1); // 다음 날짜로 이동
         }
 
-        return events;
-    }
+        // 뷰로 데이터를 전달하기 위해 모델에 "calendar" 속성 추가
+        model.addAttribute("calendar", calendar);
 
-    private int daysInMonth(int month) {
-        int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        return daysInMonth[month];
+        return "layout/calendar";
     }
 }
