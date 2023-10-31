7b64e1ae1bd19b1b9b0247f5666e88bb8b0389f5
yunjungeun <dev.byje@gmail.com>
2023-10-31 12:43:12 +0900

    1031_헤더 완료

--- src/main/java/com/example/yoonlove/controller/Main.java
+++ src/main/java/com/example/yoonlove/controller/Main.java
@@ -21,4 +21,16 @@ public class Main {
     public String testPage1(){
         return "/main/posttest";
     }
+
+
+    @GetMapping("/로그인")
+    public String testPage2(){
+        return "/main/login";
+    }
+
+    @GetMapping("/헤더")
+    public String testPage3(){
+        return "/layout/header";
+    }
 }
+
