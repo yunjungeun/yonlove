e2f75db460847680cfcac37aabbbca7ec44ceaf8
yunjungeun <dev.byje@gmail.com>
2023-11-01 19:01:32 +0900

    11/1 전체리스트 수정22 7:01

--- src/main/java/com/example/yoonlove/controller/CsController.java
+++ src/main/java/com/example/yoonlove/controller/CsController.java
@@ -63,7 +63,7 @@ public class CsController {
 
         List<NoticeDto> dto = csService.selectListNotice(pageInfo);
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("/layout/listnotice");
+        mv.setViewName("/cs/listnotice");
         mv.addObject("selectListNotice", dto);
 
 
